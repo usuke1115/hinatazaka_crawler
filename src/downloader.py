2178ad31e4d88ff4c19c78be007304126355b8fe
yuusuke <xiangzhongyoufu@gmail.com>
2026-05-09 23:02:35 +0900

    change: threadを使った高速化

--- src/downloader.py
+++ src/downloader.py
@@ -1,15 +1,20 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
 from pathlib import Path
+
 from src.http_client import fetch_binary
 
 
 def save_images(urls: list[str], save_dir: Path) -> None:
     save_dir.mkdir(parents=True, exist_ok=True)
-
-    for url in urls:
-        file_name = os.path.basename(url)
-        path = save_dir / file_name
-
-        content = fetch_binary(url)
-        path.write_bytes(content)
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        executor.map(save_image, urls, repeat(save_dir))
     print(f"✅ {len(urls)} Images Are Saved!")
+
+
+def save_image(url: str, save_dir: Path) -> None:
+    file_name = os.path.basename(url)
+    path = save_dir / file_name
+    content = fetch_binary(url)
+    path.write_bytes(content)
