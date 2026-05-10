c328b295094e1f031e6fba88340f84e1ef86058d
yuusuke <xiangzhongyoufu@gmail.com>
2026-05-10 20:04:31 +0900

    add: threadで高速化

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from src.crawler import HinatazakaBlogCrawler
-from src.crawler import set_url_for_crawl
-from src.downloader import save_images
+from concurrent.futures import ThreadPoolExecutor
+from itertools import chain
 from pathlib import Path
+
+from src.crawler import HinatazakaBlogCrawler, set_url_for_crawl
+from src.downloader import save_images
 
 
 def main():
-    blog_url = set_url_for_crawl()
-    crawler = HinatazakaBlogCrawler(blog_url)
-    img_urls = crawler.crawl_image_urls()
-    save_images(img_urls, Path("images"))
+    blog_urls = set_url_for_crawl()
+    crawler = HinatazakaBlogCrawler(blog_urls)
+    image_urls = []
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        image_urls = list(executor.map(crawler.crawl_image_urls, blog_urls))
+    save_images(list(chain.from_iterable(image_urls)), Path("images"))
 
 
 if __name__ == "__main__":
