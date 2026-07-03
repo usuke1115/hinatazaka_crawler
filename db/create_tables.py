668a717f0a06a5f5b8019eac8e76249d441780c4
yuusuke <xiangzhongyoufu@gmail.com>
2026-07-03 22:17:48 +0900

    add: テーブルが存在した時の対策をする

--- db/create_tables.py
+++ db/create_tables.py
@@ -5,7 +5,11 @@ conn = sqlite3.connect("db/url.db")
 with open("db/scripts/init.sql", encoding='utf-8') as f:
     sql = f.read()
 
-conn.executescript(sql)
-conn.commit()
-print("✅ blogs and images table have been created.")
-conn.close()
+try:
+    conn.executescript(sql)
+    conn.commit()
+    print("✅ blogs and images table have been created.")
+except sqlite3.OperationalError as e:
+    print("Tables already exists.")
+finally:
+    conn.close()
